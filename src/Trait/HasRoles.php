21e372fef81c28556c5d7e1dd2032b6e4c1d4704
Eugene <jonston85@gmail.com>
2025-09-11 14:43:28 +0300

    Add methods addRole removeRole to HasRoles.php trait

--- src/Trait/HasRoles.php
+++ src/Trait/HasRoles.php
@@ -11,7 +11,7 @@ trait HasRoles
     use HasPermissions;
 
     protected Collection $roles;
-    
+
     public function getRoles(): Collection
     {
         return $this->roles;
@@ -20,5 +20,25 @@ trait HasRoles
     public function hasRole(Role $role): bool
     {
         return $this->roles->contains($role);
+    }
+
+    public function addRole(Role $role): void
+    {
+        if (!$this->roles->contains($role)) {
+            $this->roles->add($role);
+            foreach ($role->getPermissions() as $permission) {
+                $this->addPermission($permission);
+            }
+        }
+    }
+
+    public function removeRole(Role $role): void
+    {
+        if ($this->roles->contains($role)) {
+            $this->roles->removeElement($role);
+            foreach ($role->getPermissions() as $permission) {
+                $this->removePermission($permission);
+            }
+        }
     }
 }

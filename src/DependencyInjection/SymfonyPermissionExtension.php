afe8ffbd375a6aa08e741c7dc853b768671091af
Eugene <jonston85@gmail.com>
2025-09-11 23:05:44 +0300

    Add mapping to extension

--- src/DependencyInjection/SymfonyPermissionExtension.php
+++ src/DependencyInjection/SymfonyPermissionExtension.php
@@ -8,10 +8,35 @@ use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SymfonyPermissionExtension extends Extension
+class SymfonyPermissionExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * Автоматически добавляем mapping Doctrine для сущностей бандла до загрузки конфигурации приложения.
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        if (!$container->hasExtension('doctrine')) {
+            return;
+        }
+
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => [
+                'mappings' => [
+                    'JonstonSymfonyPermission' => [
+                        'is_bundle' => false,
+                        'type' => 'attribute',
+                        'dir' => __DIR__ . '/../Entity',
+                        'prefix' => 'Jonston\\SymfonyPermission\\Entity',
+                        'alias' => 'SymfonyPermission',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
     /**
      * @throws Exception
      */
